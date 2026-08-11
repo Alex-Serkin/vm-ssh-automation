4e591aeadc310a8d310b61d898bb9fc572e8601c
Alex Serkin <nabeymordugoogly@gmail.com>
2026-08-11 10:30:53 +0300

    Update

--- offVM.bat
+++ offVM.bat
@@ -1,8 +1,9 @@
 @echo off
 echo [INFO] Shutting down virtual machine gracefully.
-"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm "UbuntuLocal" acpipowerbutton
 REM !!! ВНИМАНИЕ !!! "UbuntuLocal" заменить на название вашей ВМ 
 REM !!! АTTENTION !!!"UbuntuLocal" replace with the name of your VM
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" controlvm "UbuntuLocal" acpipowerbutton
+
 if %errorlevel% equ 0 (
     echo [OK] Shutdown signal sent.
     echo [INFO] The VM will shut down gracefully, may take a few seconds.
